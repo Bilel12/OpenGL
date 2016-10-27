887f027ea633b3b1f0e47aa2746db962a883e393
matt360 <matthewwallace1991@icloud.com>
2016-10-27 13:07:20 +0100

    Lab8.6 - skybox top face mapped. All UVs need fixes

--- Lab8/GraphicsProgramming/GraphicsProgramming/Scene.cpp
+++ Lab8/GraphicsProgramming/GraphicsProgramming/Scene.cpp
@@ -338,30 +338,30 @@ void Scene::render() {
 				///////////////////////
 				glBegin(GL_TRIANGLES); // top face
 				glNormal3f(0.0f, 1.0f, 0.0f);
-				glTexCoord2f(0, 0);
-				glVertex3f(1, 1, 1);
+				glTexCoord2f(0.5, 0);
+				glVertex3f(1, 1, 1);	// Right top
 
 				glNormal3f(0.0f, 1.0f, 0.0f);
-				glTexCoord2f(0, 1);
-				glVertex3f(1, 1, -1);
+				glTexCoord2f(0.5, 0.25);
+				glVertex3f(1, 1, -1);	// Right bottom
 
 				glNormal3f(0.0f, 1.0f, 0.0f);
-				glTexCoord2f(1, 1);
-				glVertex3f(-1, 1, -1);
+				glTexCoord2f(0.25, 0.25);
+				glVertex3f(-1, 1, -1);	// Left bottom
 				glEnd();
 
 				glBegin(GL_TRIANGLES);
 				glNormal3f(0.0f, 1.0f, 0.0f);
-				glTexCoord2f(1, 1);
-				glVertex3f(-1, 1, -1);
+				glTexCoord2f(0.25, 0.25);
+				glVertex3f(-1, 1, -1);	// Left bottom
 
 				glNormal3f(0.0f, 1.0f, 0.0f);
-				glTexCoord2f(1, 0);
-				glVertex3f(-1, 1, 1);
+				glTexCoord2f(0.25, 0);
+				glVertex3f(-1, 1, 1);	// Left top
 
 				glNormal3f(0.0f, 1.0f, 0.0f);
-				glTexCoord2f(0, 0);
-				glVertex3f(1, 1, 1);
+				glTexCoord2f(0.5, 0);
+				glVertex3f(1, 1, 1);	// Right top
 				glEnd();
 			}
 			glEnable(GL_DEPTH_TEST);
